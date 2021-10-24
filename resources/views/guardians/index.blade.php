8510c408afbd1010a7bd20ad3f547f5a51ab38ac
xed-programmer <xedprogrammer@gmail.com>
2021-10-24 13:48:10 +0800

    Modified in Guardian index view

--- resources/views/guardians/index.blade.php
+++ resources/views/guardians/index.blade.php
@@ -7,24 +7,63 @@
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h2>Students</h2>
-                    <div class="border-t border-gray-200">
-                        @foreach ($guardian->students as $i => $s)
-                            <div
-                                class="
-                                @if ($i % 2 == 0)
-                                bg-gray-50
-                                @else
-                                bg-white
-                                @endif 
-                                px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt class="text-sm font-medium text-gray-500">
-                                    <a href="{{ route('guardian.show.student', $s) }}">{{ $s->user->name }}</a>
-                                </dt>
-                                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {{ $s->student_code }}
-                                </dd>
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead class="bg-gray-50">
+                                            <tr>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    {{ __('Name') }}
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    {{ __('Student Code') }}
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    {{ __('Guardian\'s Phone') }}
+                                                </th>
+                                                <th scope="col"
+                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    {{ __('Action') }}
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                            @forelse ($guardian->students as $i => $s)
+                                                <tr>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {{ $s->user->name }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {{ $s->student_code }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {{ $s->phone }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        <a href="{{ route('guardian.show.student', $s) }}"
+                                                            class="text-xs bg-green-300 text-green-800 py-2 px-4 rounded-lg">
+                                                            {{ __('View') }}
+                                                        </a>
+                                                    </td>
+                                                </tr>
+
+                                            @empty
+                                                <tr>
+                                                    <td>
+                                                        <p class="text-gray-300 text-center">No Student</p>
+                                                    </td>
+                                                </tr>
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
