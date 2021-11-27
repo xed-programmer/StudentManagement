07ba0c15e771366cbbb381ede7bca8856698927d
xed-programmer <xedprogrammer@gmail.com>
2021-11-27 09:53:50 +0800

    Fixed bug in home page when no announcement

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $post = Post::latest()->firstOrFail();
+        $post = Post::latest()->first();          
         return view('home')->with(['post'=>$post]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,30 +43,40 @@
         </div>
     </div>
 
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="bg-gray-50">
-        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-            <h2 class="text-3xl font-extrabold text-center tracking-tight text-gray-900 sm:text-4xl">
-                <span class="block text-indigo-600">New Announcement</span>
-            </h2>
-            <div class="pt-4">
-                <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
-                        <div class="p-4 text-sm flex items-center justify-end text-gray-500 border-b border-gray-300">
-                            <i class="fas fa-clock mr-2"></i>
-                            {{ $post->created_at->diffForHumans() }}
-                        </div>
-                        <div class="p-6">
-                            {!! $post->body !!}
-                        </div>
-                        <div class="p-4 bg-gray-50 text-xs">
-                            <a href="{{ route('announcement') }}">See more announcements</a>
+    @if ($post != null)
+        <div class="bg-gray-50">
+            <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h2 class="text-3xl font-extrabold text-center tracking-tight text-gray-900 sm:text-4xl">
+                    <span class="block text-indigo-600">New Announcement</span>
+                </h2>
+                <div class="pt-4">
+                    <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
+                            <div
+                                class="p-4 text-sm flex items-center justify-end text-gray-500 border-b border-gray-300">
+                                <i class="fas fa-clock mr-2"></i>
+                                {{ $post->created_at->diffForHumans() }}
+                            </div>
+                            <div class="p-6">
+                                {!! $post->body !!}
+                            </div>
+                            <div class="p-4 bg-gray-50 text-xs">
+                                <a href="{{ route('announcement') }}">See more announcements</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="bg-gray-50">
+            <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                <h2 class="text-3xl font-extrabold text-center tracking-tight text-gray-900 sm:text-4xl">
+                    <span class="block text-gray-600">No Announcement</span>
+                </h2>
+            </div>
+        </div>
+    @endif
 
 
 </x-app-layout>
