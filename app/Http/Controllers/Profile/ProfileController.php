9a6cd2480833bab40d402320052f246b546f2b56
xed-programmer <xedprogrammer@gmail.com>
2021-11-27 16:06:28 +0800

    Added a feature where student can see whos guardian connected to them

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Models\Guardian;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -20,7 +21,10 @@ class ProfileController extends Controller
             case 'admin':
                 return view('admin.profile', ['user' => $request->user()]);
             case 'student':
-                return view('students.profile', ['user' => $request->user()]);
+                $student = Student::with('user')->whereBelongsTo($request->user())->first();                                
+                $guardians = Guardian::with('user')->whereRelation('students', 'student_id', $student->id)
+                ->get();                
+                return view('students.profile', ['student' => $student, 'guardians' => $guardians]);
             case 'guardian':
                 // $user = User::with('guardian.students.user')->where('id', $request->user()->id)->firstOrFail();                
                 return view('guardians.profile', ['user' => $request->user()]);
--- resources/views/students/profile.blade.php
+++ resources/views/students/profile.blade.php
@@ -10,13 +10,13 @@
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
-    <div class="w-1/2 mx-auto mt-2">
+    <div class="lg:w-1/2 mx-auto mt-2 sm:w-full">
         <div class="bg-white shadow overflow-hidden sm:rounded-lg">
             <div class="flex justify-between px-4 py-5 sm:px-6">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
                     Your Information
                 </h3>
-                @if (!$user->hasVerifiedEmail())
+                @if (!$student->user->hasVerifiedEmail())
                     {{-- <form method="GET" action="{{ route('profile.student.edit') }}">
                         @csrf
                         <div>
@@ -52,7 +52,7 @@
                             Full name
                         </dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $user->name }}
+                            {{ $student->user->name }}
                         </dd>
                     </div>
                     <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -60,7 +60,7 @@
                             Student Code
                         </dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $user->student->student_code }}
+                            {{ $student->student_code }}
                         </dd>
                     </div>
                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -68,7 +68,7 @@
                             Email address
                         </dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $user->email }}
+                            {{ $student->user->email }}
                         </dd>
                     </div>
                     <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -76,7 +76,61 @@
                             Guardian's Phone
                         </dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $user->student->phone }}
+                            {{ $student->phone }}
+                        </dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt class="text-sm font-medium text-gray-500">
+                            Guardians Connected to You
+                        </dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <div class="flex flex-col">
+                                <div class="overflow-x-auto">
+                                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                            <table class="min-w-full divide-y divide-gray-200">
+                                                <thead class="bg-gray-50">
+                                                    <tr>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            Name
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                            Email
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody class="bg-white divide-y divide-gray-200">
+                                                    @forelse($guardians as $guardian)
+                                                        <tr>
+                                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                                <div class="text-sm text-gray-900">
+                                                                    {{ $guardian->user->name }}
+                                                                </div>
+                                                            </td>
+                                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                                <div class="text-sm text-gray-900">
+                                                                    {{ $guardian->user->email }}
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    @empty
+                                                        <tr>
+                                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                                <div class="text-sm text-gray-500">
+                                                                    No one
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    @endforelse
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </dd>
                     </div>
                 </dl>
