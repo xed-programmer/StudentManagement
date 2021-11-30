6d9a1a7ca17f5c405d886031582707647a744b55
= <=>
2021-11-30 20:09:05 +0800

    sms

---
+++ app/Http/Controllers/SmsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Nexmo\Laravel\Facade\Nexmo;
+
+class SmsController extends Controller
+{
+    public function sendMessage(){
+      try {
+        $message = Nexmo::message()->send([
+          'to'   => '639663126435',
+          'from' => '639663126435',
+          'text' => 'SMS NOTIFICATION GOODS NA .'
+        ]);
+        echo 'sent';
+       }catch (\Exception $e) {
+        echo $e->getMessage();
+
+       }
+    }
+}
