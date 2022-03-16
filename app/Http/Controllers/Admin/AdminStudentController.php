f7db254ba714c0e8a24b73c3fa35cd521b787d29
xed-programmer <xedprogrammer@gmail.com>
2022-03-16 16:36:01 +0800

    Update student excel import

--- app/Http/Controllers/Admin/AdminStudentController.php
+++ app/Http/Controllers/Admin/AdminStudentController.php
@@ -41,8 +41,11 @@ class AdminStudentController extends Controller
     public function importStudent(Request $request)
     {        
         if($request->has('file')){
-            $file = request()->file('file')->getRealPath();            
+            //$file = request()->file('file')->getRealPath();     
+            $file1 = request()->file('file')->store('temp');                        
+            $file = storage_path('app') . '/' . $file1;            
             Excel::import(new StudentImport, $file);
+            
             if(session()->has('student_count')){                
                 $request->session()->flash('message', 'Student Import Successfully. Count ' . session()->get('student_count'));
                 $request->session()->flash('alert-class', 'alert-success');
