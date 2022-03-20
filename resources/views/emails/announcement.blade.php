3f474dfb77c09cd204a8e071d8fdcb1a267f809e
xed-programmer <xedprogrammer@gmail.com>
2022-03-20 12:41:54 +0800

    fixing announcement mail

--- resources/views/emails/announcement.blade.php
+++ resources/views/emails/announcement.blade.php
@@ -1,14 +1,13 @@
 @component('mail::message')
     # Annoucement
 
-    {{-- The body of your message. --}}
-    {{-- Body --}}
-    {!! $body !!}
+    @{{ $body }}
 
     @component('mail::button', ['url' => url('/announcements')])
         View More Announcements
     @endcomponent
 
     Thanks, <br>
-    {{ config('app.name') }}
+    {{-- {{ config('app.name') }} --}}
+    School
 @endcomponent
