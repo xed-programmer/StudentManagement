a3c865d688e4e71cf4a0c8136f656dee81f54ec3
xed-programmer <xedprogrammer@gmail.com>
2022-03-21 22:09:29 +0800

    refactored gatapass controller

--- app/Http/Controllers/GatePassController.php
+++ app/Http/Controllers/GatePassController.php
@@ -19,30 +19,23 @@ class GatePassController extends Controller
             'student_code' => ['required', 'max:10', 'exists:students,student_code'],
         ]);
 
-        $student = Student::with(['attendances', 'user'])->where('student_code', $request->student_code)->firstOrFail();                
+        $student = Student::with(['attendances' => function($q){
+            $q->where('created_at', '>', today())->latest();
+        }, 'user'])        
+        ->where('student_code', $request->student_code)->firstOrFail();
         
-        //This is the equivalent syntax in Mysql
-        //SELECT * FROM `attendances` WHERE `created_at` > '2021-10-25 00:00:00.0' AND (`status` = 'time-in' OR `status` = 'time-out');
-        $attendances = $student->attendances()
-        ->where('created_at', '>', today())
-        ->where(function ($query){
-            return $query->where('status', 'time-in')->orWhere('status', '=', 'time-out');
-        })
-        ->latest()->get();
-        
-        //check if the student tap rfid within 60 seconds
-        
-        if($attendances->count()>0){
-            if(now()->diffInSeconds($attendances[0]->created_at) < 60){                
-                return view('gatepass.index')->with('student', $student);
+        //check if the student tap rfid within 60 seconds        
+        if($student->attendances->count()>0){
+            if(now()->diffInSeconds($student->attendances[0]->created_at) > 60){                                
+                //if count is even, then it must be time in, else time out
+                if($student->attendances->count()%2 == 0){
+                    $student->attendances()->create(['status' => 'time-in']);
+                }else{
+                    $student->attendances()->create(['status' => 'time-out']);
+                }                        
             }
-        }
-        //if count is even, then it must be time in, else time out
-        if($attendances->count()%2 == 0){
-            $student->attendances()->create(['status' => 'time-in']);
-        }else{
-            $student->attendances()->create(['status' => 'time-out']);
-        }        
+        }    
+        
         return view('gatepass.index')->with(['student'=> $student]);
     }
 }
