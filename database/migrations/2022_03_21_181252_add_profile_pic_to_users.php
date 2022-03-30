c91e2fac1c942a714618c3a56761a3af154b3f76
xed-programmer <xedprogrammer@gmail.com>
2022-03-30 18:22:03 +0800

    profile_pic column set nullable to true

--- database/migrations/2022_03_21_181252_add_profile_pic_to_users.php
+++ database/migrations/2022_03_21_181252_add_profile_pic_to_users.php
@@ -14,7 +14,7 @@ class AddProfilePicToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_pic')->nullable(false);
+            $table->string('profile_pic')->nullable(true);
         });
     }
 
